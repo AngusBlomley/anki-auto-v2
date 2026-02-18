e6e63c6255d05d1ad23e0c52099d3da69c42b07c
angusblomley <ablomley01@gmail.com>
2026-02-19 01:46:03 +0900

    code formmatting. added icon package. built built the google tts api, got bare minimum api backend api working, started building out the front end

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,18 @@
-import express from "express"
-import cors from "cors"
+import textToSpeech from "@google-cloud/text-to-speech";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 
-import type { TablesInsert } from "../src/types/supabase"
+import type { TablesInsert } from "../src/types/supabase";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
+const ttsClient = new textToSpeech.TextToSpeechClient();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 interface Jisho {
   japanese: [{ word: string; reading: string }];
@@ -17,32 +22,67 @@ interface Jisho {
 
 interface Tatoeba {
   text: string;
-  translations: [{text: string}];
-  transcriptions: [{text:string}];
+  translations: [{ text: string }];
+  transcriptions: [{ text: string }];
 }
 
-const JISHO_API = "https://jisho.org/api/v1/search/words?keyword="
-const TATOEBA_API = "https://api.tatoeba.org"
+const JISHO_API = "https://jisho.org/api/v1/search/words?keyword=";
+const TATOEBA_API = "https://api.tatoeba.org";
 
 // TODO: Edge cases:
 // 1. If a user types one kanji it can have many meanings, e.g. (空 → から、そら)
 // Ans: Show all multiple word and sentence meanings
+// 2. If no kanji in sentence, reading sentence is not parsed correctly.
 
-app.post('/getData', async (req, res) => {
+app.post("/getData", async (req, res) => {
   try {
-
     // Jisho
-    const jishoRes = await fetch(`${JISHO_API}${req.body.word}`)
+    const jishoRes = await fetch(`${JISHO_API}${req.body.word}`);
     const jishoParsed = await jishoRes.json();
     const jishoObj: Jisho = jishoParsed.data[0];
 
     // Tateoba
-    const tatoebaRes = await fetch(`${TATOEBA_API}/v1/sentences?q=${req.body.word}&lang=jpn&sort=relevance&showtrans:lang=eng&include=transcriptions`)
+    const tatoebaRes = await fetch(
+      `${TATOEBA_API}/v1/sentences?q=${req.body.word}&lang=jpn&sort=relevance&showtrans:lang=eng&include=transcriptions`,
+    );
     const tatoebaParsed = await tatoebaRes.json();
     const tatoebaObj: Tatoeba = tatoebaParsed.data[0];
 
     // Parse tatoeba furigana to hiragana. e.g. "お[前|まえ]はバナナ[人|じん]だ。" **REVISE**
-    const readingSentence = tatoebaObj.transcriptions[0].text.replace(/\[.+?\|(.+?)\]/g, "$1");
+    const readingSentence = tatoebaObj.transcriptions[0].text.replace(
+      /\[.+?\|(.+?)\]/g,
+      "$1",
+    );
+
+    const [ttsResWord] = await ttsClient.synthesizeSpeech({
+      input: { text: jishoObj.japanese[0].reading },
+      voice: {
+        languageCode: "ja-JP",
+        name: "ja-JP-Neural2-B",
+      },
+      audioConfig: {
+        audioEncoding: "MP3",
+      },
+    });
+
+    const [ttsResSentence] = await ttsClient.synthesizeSpeech({
+      input: { text: tatoebaObj.text },
+      voice: {
+        languageCode: "ja-JP",
+        name: "ja-JP-Neural2-B",
+      },
+      audioConfig: {
+        audioEncoding: "MP3",
+      },
+    });
+
+    const audioWord = Buffer.from(
+      ttsResWord.audioContent as Uint8Array,
+    ).toString("base64");
+
+    const audioSentence = Buffer.from(
+      ttsResSentence.audioContent as Uint8Array,
+    ).toString("base64");
 
     const builtResponse: TablesInsert<"card"> = {
       word: jishoObj.japanese[0].word,
@@ -52,17 +92,16 @@ app.post('/getData', async (req, res) => {
       reading_sentence: readingSentence,
       english_sentence: tatoebaObj.translations[0].text,
       jlpt: jishoObj.jlpt.join(", "),
-      sentence_audio_url: "",
-      word_audio_url: "",
-    }
+      sentence_audio_url: audioSentence,
+      word_audio_url: audioWord,
+    };
 
     console.log(builtResponse);
     res.json(builtResponse);
-    
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'something went wrong' })
+    console.error(error);
+    res.status(500).json({ error: "something went wrong" });
   }
-})
+});
 
-app.listen(port, () => {})
+app.listen(port, () => {});
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
+import { Play } from "lucide-react";
 import Header from "./components/Header";
+import Spinner from "./components/Spinner";
 
-interface JishoResponse {
-  japanese: [{ word: string; reading: string }];
-  senses: [{ english_definitions: string[] }];
-  jlpt: string[];
-}
+import type { TablesInsert } from "./types/supabase";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState<TablesInsert<"card">[]>([]);
   const [input, setInput] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isEditingId, setIsEdtingId] = useState<number>(0);
 
   const onSubmit = () => {
     const fetchData = async (data: string) => {
+      setLoading(true);
       try {
         const response = await fetch("http://localhost:3000/getData", {
           method: "POST",
@@ -20,12 +22,20 @@ function App() {
         });
         const jsonResponse = await response.json();
         console.log(jsonResponse);
+        setData(() => [jsonResponse]);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData(input);
+  };
+
+  const playAudio = (base64Audio: string) => {
+    const audio = new Audio(`data:audio/mp3;base64,${base64Audio}`);
+    audio.play();
   };
 
   return (
@@ -44,18 +54,81 @@ function App() {
           />
           <button
             className="p-2 rounded cursor-pointer hover:bg-gray-200 bg-white text-black"
-            onClick={onSubmit}>
+            onClick={onSubmit}
+          >
             Generate Cards
           </button>
         </div>
 
-        {/* Display */}
+        {/*
+          The following <main> data map is invalid, this is mapping over the entire built api.
+          This response needs to be split into 4 cards of different types and put
+          into an array, then mapped over and displayed.
+          */}
 
-        {/* table for displaying cards, 1 card per row, 4 cols: [kanji, reading, engilsh, crud] */}
-        <table></table>
+        <main className="flex justify-center">
+          {loading ? (
+            <Spinner />
+          ) : (
+            <div>
+              {data.map((item, i) => (
+                <div className="grid grid-cols-2 grid-rows-2 gap-4" key={i}>
+                  <div className="relative flex border border-gray-400 rounded-md min-h-82 min-w-64">
+                    <div className="absolute flex justify-center text-sm text-gray-300 font-extralight top-0 w-full rounded-t-md align-top p-2 border-b border-gray-400">
+                      Listen
+                    </div>
+                    <div className="flex justify-center items-center h-full w-full py-3">
+                      <button
+                        className="cursor-pointer rounded-full p-1 bg-gray-300"
+                        onClick={() => playAudio(item.word_audio_url!)}
+                      >
+                        <Play color="black" fill="black" />
+                      </button>
+                    </div>
+                    <button className="absolute flex justify-center text-sm text-gray-300 font-extralight bottom-0 w-full rounded-b-md align-bottom p-2 border-t border-gray-400 hover:bg-gray-950 cursor-pointer">
+                      Front
+                    </button>
+                  </div>
+                  <div className="relative flex border border-gray-400 rounded-md min-h-82 min-w-64">
+                    <div className="absolute flex justify-center text-sm text-gray-300 font-extralight top-0 w-full rounded-t-md align-top p-2 border-b border-gray-400">
+                      Word
+                    </div>
+                    <div className="flex justify-center items-center h-full w-full py-3"></div>
+                    <button className="absolute flex justify-center text-sm text-gray-300 font-extralight bottom-0 w-full rounded-b-md align-bottom p-2 border-t border-gray-400 hover:bg-gray-950 cursor-pointer">
+                      Front
+                    </button>
+                  </div>
+                  <div className="relative flex border border-gray-400 rounded-md min-h-82 min-w-64">
+                    <div className="absolute flex justify-center text-sm text-gray-300 font-extralight top-0 w-full rounded-t-md align-top p-2 border-b border-gray-400">
+                      Sentence
+                    </div>
+                    <div className="flex justify-center items-center h-full w-full py-3"></div>
+                    <button className="absolute flex justify-center text-sm text-gray-300 font-extralight bottom-0 w-full rounded-b-md align-bottom p-2 border-t border-gray-400 hover:bg-gray-950 cursor-pointer">
+                      Front
+                    </button>
+                  </div>
+                  <div className="relative flex border border-gray-400 rounded-md min-h-82 min-w-64">
+                    <div className="absolute flex justify-center text-sm text-gray-300 font-extralight top-0 w-full rounded-t-md align-top p-2 border-b border-gray-400">
+                      Recall
+                    </div>
+                    <div className="flex justify-center items-center h-full w-full py-3"></div>
+                    <button className="absolute flex justify-center text-sm text-gray-300 font-extralight bottom-0 w-full rounded-b-md align-bottom p-2 border-t border-gray-400 hover:bg-gray-950 cursor-pointer">
+                      Front
+                    </button>
+                  </div>
+
+                  <button
+                    className="cursor-pointer"
+                    onClick={() => playAudio(item.sentence_audio_url!)}
+                  >
+                    <Play />
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+        </main>
       </div>
     </>
   );
 }
-
-export default App;
