4ddac48a588316d0c8a354f6770443c3f0e3976e
angusblomley <ablomley01@gmail.com>
2026-02-19 18:03:57 +0900

    refactor and update formatting, build table on front end

--- backend/server.ts
+++ backend/server.ts
@@ -26,33 +26,27 @@ interface Tatoeba {
   transcriptions: [{ text: string }];
 }
 
+// API URLS
 const JISHO_API = "https://jisho.org/api/v1/search/words?keyword=";
 const TATOEBA_API = "https://api.tatoeba.org";
 
 // TODO: Edge cases:
 // 1. If a user types one kanji it can have many meanings, e.g. (空 → から、そら)
 // Ans: Show all multiple word and sentence meanings
-// 2. If no kanji in sentence, reading sentence is not parsed correctly.
 
 app.post("/getData", async (req, res) => {
   try {
-    // Jisho
+    // Get Jisho
     const jishoRes = await fetch(`${JISHO_API}${req.body.word}`);
     const jishoParsed = await jishoRes.json();
     const jishoObj: Jisho = jishoParsed.data[0];
 
-    // Tateoba
-    const tatoebaRes = await fetch(
-      `${TATOEBA_API}/v1/sentences?q=${req.body.word}&lang=jpn&sort=relevance&showtrans:lang=eng&include=transcriptions`,
-    );
+    // Get Tateoba
+    const tatoebaRes = await fetch(`${TATOEBA_API}/v1/sentences?q=${req.body.word}&lang=jpn&sort=relevance&showtrans:lang=eng&include=transcriptions`,);
     const tatoebaParsed = await tatoebaRes.json();
     const tatoebaObj: Tatoeba = tatoebaParsed.data[0];
-
-    // Parse tatoeba furigana to hiragana. e.g. "お[前|まえ]はバナナ[人|じん]だ。" **REVISE**
-    const readingSentence = tatoebaObj.transcriptions[0].text
-      .replace(/\[.+?\|(.+?)\]/g, "$1")
-      .replace(/\|/g, "");
-
+    
+    // Get TTS // TODO: Eliminate DRY code
     const [ttsResWord] = await ttsClient.synthesizeSpeech({
       input: { text: jishoObj.japanese[0].reading },
       voice: {
@@ -74,7 +68,22 @@ app.post("/getData", async (req, res) => {
         audioEncoding: "MP3",
       },
     });
+    
+    // Format Data
+    const cleanWord = jishoObj.japanese[0].word;
+    
+    // - Capitalise en definitions
+    const sliceEnWords = jishoObj.senses[0].english_definitions.slice(0,2);
+    const cleanEnWords = sliceEnWords.map((c) => c[0].toUpperCase() + c.slice(1)).join(", ");
+    
+    const cleanEnSentence = tatoebaObj.translations[0].text;
+    const cleanJlpt = jishoObj.jlpt.join(", ");
+    const cleanReadingWord = jishoObj.japanese[0].reading;
+    
+    // - Parse tatoeba furigana to hiragana. e.g. "お[前|まえ]はバナナ[人|じん]だ。" **REVISE**
+    const cleanReadingSentence = tatoebaObj.transcriptions[0].text.replace(/\[.+?\|(.+?)\]/g, "$1").replace(/\|/g, "");
 
+    // - Convert TTS to base64 string
     const audioWord = Buffer.from(
       ttsResWord.audioContent as Uint8Array,
     ).toString("base64");
@@ -84,18 +93,17 @@ app.post("/getData", async (req, res) => {
     ).toString("base64");
 
     const builtResponse: TablesInsert<"card"> = {
-      word: jishoObj.japanese[0].word,
-      reading_word: jishoObj.japanese[0].reading,
-      english_word: jishoObj.senses[0].english_definitions.join(", "),
+      word: cleanWord,
+      reading_word: cleanReadingWord,
+      english_word: cleanEnWords,
       sentence: tatoebaObj.text,
-      reading_sentence: readingSentence,
-      english_sentence: tatoebaObj.translations[0].text,
-      jlpt: jishoObj.jlpt.join(", "),
-      sentence_audio_url: audioSentence,
+      reading_sentence: cleanReadingSentence,
+      english_sentence: cleanEnSentence,
+      jlpt: cleanJlpt,
       word_audio_url: audioWord,
+      sentence_audio_url: audioSentence,
     };
 
-    console.log(builtResponse);
     res.json(builtResponse);
   } catch (error) {
     console.error(error);
--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Spinner from "./components/Spinner";
 import Card from "./components/Card";
 
 import type { TablesInsert } from "./types/supabase";
+import { X } from "lucide-react";
 
 type Word = Omit<TablesInsert<"card">, "type">;
 
 export default function App() {
   const [cards, setCards] = useState<TablesInsert<"card">[]>([]);
-  // const [word, setWord] = useState<Word>();
+  const [words, setWords] = useState<Word[]>([]);
   const [input, setInput] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [flipped, setFlipped] = useState<boolean[]>([]);
@@ -32,10 +33,10 @@ export default function App() {
         { ...wordData, type: "sentence" },
         { ...wordData, type: "recall" },
       ];
-
-      console.log(wordData);
       setCards(cards);
       setFlipped(new Array(cards.length).fill(false));
+      setWords((prev) => [...prev, { ...wordData }]);
+      console.log(wordData);
     } catch (error) {
       console.error(error);
     } finally {
@@ -48,7 +49,7 @@ export default function App() {
     audio.play();
   };
 
-  // TODO: **Revise**
+  // REDO
   const toggleSide = (i: number) => {
     setFlipped((prev) => prev.map((v, index) => (index === i ? !v : v)));
   };
@@ -80,45 +81,75 @@ export default function App() {
           {loading ? (
             <Spinner />
           ) : (
-            <div className="grid grid-cols-2 gap-4">
-              {cards.map((card, i) => (
-                <div className="flex max-w-3xl" key={i}>
-                  <div className="relative flex bg-[#141414] border border-[#404040] rounded-md min-h-82 min-w-72">
-                    <div className="absolute flex justify-center text-xs text-gray-300 top-0 w-full rounded-t-md align-top p-2 border-b border-[#404040] ">
-                      {card.type?.toUpperCase()}
+            <div>
+              <div className="flex justify-between pb-4 min-w-xl">
+                <h2 className="text-xl">Preview</h2>
+                <button>
+                  <X
+                    className="cursor-pointer hover:opacity-50"
+                    onClick={() => setCards([])}
+                  />
+                </button>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                {cards.map((card, i) => (
+                  <div className="flex max-w-3xl" key={i}>
+                    <div className="relative flex bg-[#141414] border border-[#404040] rounded-md min-h-82 min-w-72">
+                      <div className="absolute flex justify-center text-xs text-gray-300 top-0 w-full rounded-t-md align-top p-2 border-b border-[#404040] ">
+                        {card.type?.toUpperCase()}
+                      </div>
+                      <div className="flex flex-col items-center space-y-4 w-full py-10">
+                        <Card
+                          card={card}
+                          isFlipped={flipped[i]}
+                          playAudio={() =>
+                            playAudio(
+                              card.type === "sentence"
+                                ? card.sentence_audio_url!
+                                : card.word_audio_url!,
+                            )
+                          }
+                        />
+                      </div>
+                      <div className="absolute bottom-8 border-[#404040]  border-t w-full"></div>
+                      <button
+                        onClick={() => toggleSide(i)}
+                        className="absolute flex justify-center text-xs text-gray-300 opacity-50 hover:opacity-100 bottom-0 w-full rounded-b-md align-bottom p-2 hover:bg-[#121212] cursor-pointer"
+                      >
+                        {flipped[i] ? "Back" : "Front"}
+                      </button>
                     </div>
-                    <div className="flex flex-col items-center space-y-4 w-full py-10">
-                      <Card
-                        card={card}
-                        isFlipped={flipped[i]}
-                        playAudio={() =>
-                          playAudio(
-                            card.type === "sentence"
-                              ? card.sentence_audio_url!
-                              : card.word_audio_url!,
-                          )
-                        }
-                      />
-                    </div>
-                    <div className="absolute bottom-8 border-[#404040]  border-t w-full"></div>
-                    <button
-                      onClick={() => toggleSide(i)}
-                      className="absolute flex justify-center text-xs text-gray-300 opacity-50 hover:opacity-100 bottom-0 w-full rounded-b-md align-bottom p-2 hover:bg-[#121212] cursor-pointer"
-                    >
-                      {flipped[i] ? "Back" : "Front"}
-                    </button>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           )}
         </div>
 
         {/* List of words */}
-        <div>
-          {/*{words.map((word) => (
-
-          ))}*/}
+        <div className="flex justify-center w-full">
+          <table className="min-w-xl text-left">
+            {/*{words.length > 0 && (*/}
+            <thead>
+              <tr className="border-b border-[#404040]">
+                <th className="p-1">Word</th>
+                <th>Reading</th>
+                <th>Translation</th>
+                <th>Manage</th>
+              </tr>
+            </thead>
+            <tbody>
+              {words.map((word, i) => (
+                <tr key={i} className="border-b border-[#404040]">
+                  <td className="p-1">{word.word}</td>
+                  <td>{word.reading_word}</td>
+                  <td>{word.english_word}</td>
+                  <td></td>
+                </tr>
+              ))}
+            </tbody>
+            {/*)}*/}
+          </table>
         </div>
       </div>
     </>
